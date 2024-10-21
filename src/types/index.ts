5e6839b2b6bf1579d71293f3f8a7e24cde9adda5
Melissandra666 <157244568+Melissandra666@users.noreply.github.com>
2024-10-22 04:16:02 +0900

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,59 +1,64 @@
-export interface IProduct {
-    id: string;
-    title: string;
-    description: string;
-    category: string;
-    price: number;
-    image: string;
+//Интерфейс и типы данных, описывающих карточку товара
+
+export interface IProductItem {
+	id: string;
+	title: string;
+	image: string;
+	category: string;
+	description: string;
+	price: number | null;
 }
 
-export interface IOrder {
-    items: string[];
-    total: number;
-    email: string;
-    phone: string;
-    address: string;
-    payment: string;
+export type IBasketProduct = Pick<IProductItem, 'id' | 'title' | 'price'>;
+export type ICatalogProduct = Pick<
+	IProductItem,
+	'id' | 'title' | 'price' | 'image' | 'category'
+>;
+
+
+//Интерфейс и типы данных, описывающих данные покупателя
+
+export interface IUserInfo {
+	payment: string;
+	address: string;
+	email: string;
+	phone: string;
 }
 
+
+//Интерфейс, описывающий поля форм заказа товара и объеденяющий поля
+
+export type IDeliveryForm = Pick<IUserInfo, 'payment' | 'address'>;
+export type IContactsForm = Pick<IUserInfo, 'email' | 'phone'>;
+export type IOrderForm = IDeliveryForm & IContactsForm;
+
+//Интерфейс описывающий оформление заказа
+
+export interface IOrder extends IOrderForm {
+	items: string[];
+	total: number;
+}
+
+//Интерфейс описывающий результат оформления заказа
+
+export interface IOrderResult {
+	id: string;
+	total: number;
+}
+export type ISuccess = Pick<IOrderResult, 'total'>;
+
+
+//Тип, описывающий ошибки валидации форм
 export type FormErrors = Partial<Record<keyof IOrder, string>>;
 
-export interface IOrderSuccess {
-    id: string;
-    total: number;
-}
 
-export interface IOrderResult extends IOrder {
-    id: string;
-    errors?: string;
-}
-
-export interface IShopAPI {
-    getProductList: () => Promise<IProduct[]>;
-    getProduct: (id: string) => Promise<IProduct>;
-    createOrder: (order: IOrder) => Promise<IOrderSuccess>;
-}
+//Интерфейс, для хранения актуального состояния приложения
 
 export interface IAppState {
-    catalog: IProduct[];
-    selectedProduct: IProduct | null;
-    order: IOrder | null;
-    basket: string[] | null;
-    preview: string | null;
-    formErrors: FormErrors;
-
-    // API
-    setCatalog(): Promise<IProduct[]>;
-    orderProduct(): Promise<IOrderSuccess>;
-
-    // Дополнительные методы
-    clearBasket(): void;
-    clearOrder(): void;
-    validateOrder(data: FormErrors): boolean;
-
-    // Действия пользователя
-    selectProduct(id: string): void;
-    addProduct(id: string): void;
-    removeProduct(id: string): void;
-
+	catalog: ICatalogProduct[];
+	basket: IBasketProduct[];
+	preview: string | null;
+	order: IOrder | null;
+	orderResponse: IOrderResult | null;
+	loading: boolean;
 }
